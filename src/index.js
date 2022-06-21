56786066decb4c45d12b81fb5f164b4f482ed250
Jaswanth Karangula <jaswanth@Jaswanths-MacBook-Pro.local>
2022-06-21 15:34:46 +0530

    Creating Basic Elements

--- src/index.js
+++ src/index.js
@@ -1,2 +1,53 @@
 import './index.css';
 
+import React from "react";
+
+import ReactDOM from "react-dom";
+
+
+// const reactElement = React.createElement("h1",null,"From React");
+// console.log(reactElement);
+// ReactDOM.render(reactElement,document.querySelector("#root"));
+
+
+
+
+// const counterDisplayElement = React.createElement("h1", {className: "counter-display"}, 0);
+
+// const incrementButtonElement = React.createElement("button", null, "+");
+
+// const decrementButtonElement = React.createElement("button", null, "-");
+
+// const containerDivElement = React.createElement("div", null, [counterDisplayElement, incrementButtonElement, decrementButtonElement]);
+
+
+// console.log(containerDivElement);
+
+
+
+
+
+// const counterDisplayElementJSX = (<h1 className="counter-display">0</h1>)
+// const incrementButtonElementJSX = (<button>+</button>);
+// const decrementButtonElementJSX = (<button>-</button>);
+// const containerDivElementJSX = (<div>{[counterDisplayElementJSX, incrementButtonElementJSX, decrementButtonElementJSX]}</div>);
+
+
+
+
+const containerDivElementJSX = (
+
+    <div>
+  
+      <h1 className="counter-display">0</h1>
+  
+      <button>+</button>
+  
+      <button>-</button>
+  
+    </div>
+  
+  )
+  
+
+ReactDOM.render(containerDivElementJSX, document.querySelector("#root"));
